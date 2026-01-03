30d586646dcd52522334fec264fc327168408eba
Stahli72 <christian.stahl1@t-online.de>
2026-01-03 15:01:05 +0100

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,15 @@
-
+// Sofort aktiv werden
 self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+// Neue Version sofort übernehmen
 self.addEventListener("activate", event => {
   clients.claim();
 });
 
+// Immer die neueste Datei laden
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
+
